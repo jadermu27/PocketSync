02d493d45049e64a7c78cd4418bd9515366b2ca1
jadermu27 <jadermu2702@gmail.com>
2025-10-17 10:53:47 -0500

    Archivos juntos

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+//BLOQUE: JAVASCRIPT PARA EL MENU HAMBURGUESA
+//Funcionalidad para el menu hamburguesa
+
 document.addEventListener('DOMContentLoaded', function() {
     const navToggle = document.getElementById('navtoggle');
     const navMenu = document.getElementById('navmenu');
     const navToggleLines = document.querySelectorAll('.nav__toggle-line');
 
+    //Evento click para el botón hamburguesa
     navToggle.addEventListener('click', function() {
+        //Alternar la clase activa para mostrar/ocultar el menú
         navMenu.classList.toggle('nav__menu--active');
         
-        navToggleLines[0].classList.toggle('nav__toggle-line--rotate-down');
+        //Animar las líneas del botón hamburguesa
+        navToggleLines[0].classList.toggle('nav__toggle-line--top-active');
         navToggleLines[1].classList.toggle('nav__toggle-line--middle-active');
         navToggleLines[2].classList.toggle('nav__toggle-line--bottom-active');
-    }   );
+    });
 });

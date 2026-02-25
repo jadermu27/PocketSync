568283357580eba2eb9944494af590b3a7935b9c
jadermu27 <jadermu2702@gmail.com>
2026-02-25 10:14:26 -0500

    este es mi Navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App"> 
+      <Navbar />
+      <h1>Bienvenido a PocketSync</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <div className="navbar-logo">
+            🔥PocketSync
+        </div>
+
+        <ul className="navbar-links">
+            <li><a href="#inicio">Inicio</a></li>
+            <li><a href="#servicios">Reportes</a></li>
+            <li><a href="#contacto">Grupos</a></li>
+            <li><a href="#portafolio">Perfil</a></li>
+            <li>
+                <a href="#login" className="login-btn">
+                    Login
+                </a>
+                
+            </li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
